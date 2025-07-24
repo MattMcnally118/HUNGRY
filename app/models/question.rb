af37a47157ef7cf1012ae8fabbdfbb15bd2ef684
Matt McNally <matthewmcnally18@gmail.com>
2025-07-24 13:16:54 +0200

    Got ai up and running

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,22 +1,15 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :restaurant
-  attr_accessor :budget, :number_of_people, :current_mood, :dietary_restrictions, :cuisine_type
+  belongs_to :restaurant, foreign_key: :recommended_restaurant_id, optional: true
+  attr_accessor :price_range, :number_of_people, :current_mood, :dietary_restrictions, :cuisine_type
 
-  before_save :preferences
-
-  # client = OpenAI::Client.new
-  # chatgpt_response = client.chat(parameters: {
-  #   model: "gpt-4o-mini",
-  #   input: [{ role: "user", content: "Reccomend one restaurant from #{@restaurants.all} that suits the responses best from #{@question.preferences}. Return only the instance of the restaurant model chosen'."}]
-  # })
-  # @content = chatgpt_response["choices"][0]["message"]["content"]
+  before_validation :preferences
 
   private
 
   def preferences
     self.preference_data = {
-      budget: budget,
+      price_range: price_range,
       number_of_people: number_of_people,
       current_mood: current_mood,
       dietary_restrictions: dietary_restrictions,
