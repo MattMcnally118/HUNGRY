63ee4ba843376de1f60d7d6aee7ba6d63b512815
Franck Otsouka <franckengodard@gmail.com>
2025-07-24 16:43:01 +0200

    show-page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,16 @@
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: %w[show]
-
   def index
-    @restaurants = Restaurant.all
-    # put geocoding for maps
+    @restaurants = Restaurant.geocoded
+
+    @markers = @restaurants.geocoded.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: { restaurant: restaurant }),
+        marker_html: render_to_string(partial: "marker", locals: { restaurant: restaurant })
+      }
+    end
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_23_094018) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_23_102443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_23_094018) do
     t.integer "number_of_people"
     t.string "mood"
     t.text "dietary_restrictions", default: [], array: true
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
